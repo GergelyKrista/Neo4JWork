4f99d12ef4abbe982db80f7ef016267af3deb63b
GergelyKrista <gergely.krista@gmail.com>
2024-03-28 12:22:42 +0000

    Added security config

---
+++ spring-boot-neo4j/src/main/java/dev/gergely/springbootneo4j/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package dev.gergely.springbootneo4j.config;
+
+import dev.gergely.springbootneo4j.services.NeoUserDetailsService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final NeoUserDetailsService neoUserDetailsService;
+
+    public SecurityConfig(NeoUserDetailsService neoUserDetailsService) {
+        this.neoUserDetailsService = neoUserDetailsService;
+    }
+
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors((Customizer.withDefaults()))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "api/v1/auth/me"
+                        ).authenticated()
+                        .anyRequest().permitAll()
+                )
+                .userDetailsService(neoUserDetailsService)
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
