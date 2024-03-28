93708c8b414e56fffa26e49f8f9a7d94b4e3b7e7
GergelyKrista <gergely.krista@gmail.com>
2024-03-28 15:12:31 +0000

    security config updated with cors config

--- spring-boot-neo4j/src/main/java/dev/gergely/springbootneo4j/config/SecurityConfig.java
+++ spring-boot-neo4j/src/main/java/dev/gergely/springbootneo4j/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package dev.gergely.springbootneo4j.config;
 
 import dev.gergely.springbootneo4j.services.NeoUserDetailsService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +25,7 @@ public class SecurityConfig {
         this.neoUserDetailsService = neoUserDetailsService;
     }
 
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -32,5 +40,23 @@ public class SecurityConfig {
                 .userDetailsService(neoUserDetailsService)
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        //TO DO: make sure that the origins list comes from an environment file
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "httpL//127.0.0.1:3000"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "OPTIONS", "HEAD", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Reuqest-type"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("X-Get-Header"));
+        corsConfiguration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return urlBasedCorsConfigurationSource;
+
     }
 }
