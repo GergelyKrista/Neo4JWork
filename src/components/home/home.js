52b1ee3494b420735ff51585d1c3cee14182047c
GergelyKrista <gergely.krista@gmail.com>
2024-03-29 12:18:44 +0000

    HeroImage added

---
+++ src/components/home/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroImage from '../../images/School.jpg'
+
+const home = () => {
+    return (
+        <div>
+            <img src={HeroImage} alt=""/>
+        </div>
+    )
+}
+
+export default home;
