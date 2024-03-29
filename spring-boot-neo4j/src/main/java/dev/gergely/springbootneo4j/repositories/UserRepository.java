0f8fc9b2e6d94603e0731a551667e91fc2ec9194
GergelyKrista <gergely.krista@gmail.com>
2024-03-29 09:56:41 +0000

    Enrolment class added

--- spring-boot-neo4j/src/main/java/dev/gergely/springbootneo4j/repositories/UserRepository.java
+++ spring-boot-neo4j/src/main/java/dev/gergely/springbootneo4j/repositories/UserRepository.java
@@ -1,10 +1,20 @@
 package dev.gergely.springbootneo4j.repositories;
 
 import dev.gergely.springbootneo4j.models.User;
+import dev.gergely.springbootneo4j.queryresults.CourseEnrolmentQueryResult;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends Neo4jRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
+
+    @Query("MATCH (use:User), (course:Course) WHERE user.username = $username AND course.identifier = $identifier " +
+            "RETURN EXISTS((user)-[:ENROLLED_IN]->(course))")
+    Boolean findEnrolmentStatus(String username, String identifier);
+
+    @Query("MATCH (use:User), (course:Course) WHERE user.username = $username AND course.identifier = $identifier " +
+            "CREATE (user)-[:ENROLLED_IN]->(course) RETURN user, course")
+    CourseEnrolmentQueryResult createEnrolmentRelationship(String username, String identifier);
 }
